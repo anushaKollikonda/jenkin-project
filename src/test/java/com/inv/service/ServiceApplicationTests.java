f4c138b4293ec1092bf956022e92542d2eb575e0
anushakollikonda <anushakollikonda@gmail.com>
2023-09-11 11:14:35 +0530

    my app with .gitignore

---
+++ src/test/java/com/inv/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inv.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
